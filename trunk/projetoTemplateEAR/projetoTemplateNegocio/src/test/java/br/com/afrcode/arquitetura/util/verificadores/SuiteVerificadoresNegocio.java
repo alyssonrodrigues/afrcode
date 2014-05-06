974fedc93e33dcac7bc048b35d4d0caaddd9f74f
alysson.rodrigues@gmail.com <alysson.rodrigues@gmail.com@4c46430c-79e8-fb40-bfba-8e12dd562e6e>
2014-05-06 00:44:43 +0000

    Initial import.

---
+++ trunk/projetoTemplateEAR/projetoTemplateNegocio/src/test/java/br/com/afrcode/arquitetura/util/verificadores/SuiteVerificadoresNegocio.java
@@ -0,0 +1,10 @@
+package br.com.afrcode.arquitetura.util.verificadores;
+
+/**
+ * Suite de verificadores de aderência.
+ * 
+ * 
+ */
+public class SuiteVerificadoresNegocio extends SuiteVerificadoresNegocioArqSist {
+
+}
