8fb49745b74cf6537aa407a7986713f6422d4046
alysson.rodrigues@gmail.com <alysson.rodrigues@gmail.com@4c46430c-79e8-fb40-bfba-8e12dd562e6e>
2014-06-27 04:42:17 +0000

    Ajustes de impl.

---
+++ trunk/fw_arquitetura/fw_arquitetura_sist/src/test/java/br/com/afrcode/arquitetura/is/util/ejb/Caller.java
@@ -0,0 +1,13 @@
+package br.com.afrcode.arquitetura.is.util.ejb;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Caller utilitário para execução de TUs de EJBs segundo documentação
+ * http://tomee.apache.org/unit-testing-transactions.html.
+ */
+public interface Caller {
+
+	public <V> V call(Callable<V> callable) throws Exception;
+
+}
