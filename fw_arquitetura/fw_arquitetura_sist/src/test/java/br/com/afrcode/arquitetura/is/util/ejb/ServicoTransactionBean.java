85cc1b19a67442e98356af426b6093efbd8980b1
alysson.rodrigues@gmail.com <alysson.rodrigues@gmail.com@4c46430c-79e8-fb40-bfba-8e12dd562e6e>
2014-06-27 04:42:17 +0000

    Ajustes de impl.

---
+++ fw_arquitetura/fw_arquitetura_sist/src/test/java/br/com/afrcode/arquitetura/is/util/ejb/ServicoTransactionBean.java
@@ -0,0 +1,40 @@
+package br.com.afrcode.arquitetura.is.util.ejb;
+
+import java.util.concurrent.Callable;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import br.com.afrcode.arquitetura.is.util.excecao.ExcecaoNaoPrevistaRemota;
+
+/**
+ * EJB utilitário para execução de TUs de EJBs segundo documentação
+ * http://tomee.apache.org/unit-testing-transactions.html.
+ */
+@Stateless
+@Remote(Caller.class)
+public class ServicoTransactionBean extends AbstractServicoEJB implements
+		Caller {
+	@Autowired
+	private TransactionTemplate transactionTemplate;
+
+	@Override
+	public <V> V call(final Callable<V> callable) throws Exception {
+		return transactionTemplate.execute(new TransactionCallback<V>() {
+			@Override
+			public V doInTransaction(TransactionStatus status) {
+				try {
+					return callable.call();
+				} catch (Exception e) {
+					throw new ExcecaoNaoPrevistaRemota(e);
+				}
+			}
+		});
+	}
+
+}
---
+++ fw_arquitetura/fw_arquitetura_sist/src/test/java/br/com/afrcode/arquitetura/is/util/ejb/ServicoTransactionEjbProxyBeansConfig.java
@@ -0,0 +1,24 @@
+package br.com.afrcode.arquitetura.is.util.ejb;
+
+import javax.naming.NamingException;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+
+import br.com.afrcode.arquitetura.is.spring.config.ejb.AbstractEjbProxyBeansConfig;
+import br.com.afrcode.arquitetura.spring.config.util.Profiles;
+
+@Configuration
+@Profile(Profiles.PROFILE_TU)
+public class ServicoTransactionEjbProxyBeansConfig extends
+		AbstractEjbProxyBeansConfig<Caller> {
+
+	@Bean
+	@Lazy
+	public Caller servicoTransactionBeanRemote() throws NamingException {
+		return criarEjb3RemoteStatelessSessionProxy("ServicoTransactionBeanRemote");
+	}
+
+}
