ff53f888a204bcace90611bed59a2b0363ee2551
alysson.rodrigues@gmail.com <alysson.rodrigues@gmail.com@4c46430c-79e8-fb40-bfba-8e12dd562e6e>
2014-07-01 22:15:16 +0000

    Ajustes impl.

---
+++ trunk/fw_arquitetura/fw_arquitetura_sist/src/main/java/br/com/afrcode/arquitetura/is/util/ejb/EjbContextUtils.java
@@ -0,0 +1,34 @@
+package br.com.afrcode.arquitetura.is.util.ejb;
+
+import java.security.Principal;
+
+import javax.ejb.EJBContext;
+
+import org.apache.log4j.Logger;
+
+public class EjbContextUtils {
+	private static final String USUARIO_NAO_AUTENTICADO = "SEGURANCA";
+	private static final Logger LOG = Logger.getLogger(EjbContextUtils.class);
+
+	public static String obterCredencial(EJBContext ejbContext) {
+		Principal callerPrincipal = ejbContext.getCallerPrincipal();
+		String usuario = null;
+
+		if (callerPrincipal != null) {
+			String name = callerPrincipal.getName();
+			if (name != null && !"anonymous".equalsIgnoreCase(name)
+					&& !"guest".equalsIgnoreCase(name)
+					&& !"Unauthenticated".equalsIgnoreCase(name)) {
+				usuario = name;
+			}
+		}
+
+		if (usuario == null) {
+			usuario = USUARIO_NAO_AUTENTICADO;
+			LOG.warn("Usuário não autenticado, possível acesso a recurso que não exige autenticação, ex.: página de login!");
+		}
+
+		return usuario;
+	}
+
+}
