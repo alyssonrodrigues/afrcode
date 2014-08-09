738e7d7f1fdcac85c1a3ba079dfe9d2270499459
alysson.rodrigues@gmail.com <alysson.rodrigues@gmail.com@4c46430c-79e8-fb40-bfba-8e12dd562e6e>
2014-08-09 12:42:03 +0000

    Ajustes.

--- fw_arquitetura/fw_arquitetura_sist/src/main/java/br/com/afrcode/arquitetura/is/util/ejb/AutenticadorInterceptor.java
+++ fw_arquitetura/fw_arquitetura_sist/src/main/java/br/com/afrcode/arquitetura/is/util/ejb/AutenticadorInterceptor.java
@@ -7,16 +7,13 @@ import javax.ejb.EJBContext;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.InvocationContext;
 
-import org.apache.commons.lang.Validate;
 import org.apache.log4j.Logger;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import br.com.afrcode.arquitetura.is.spring.config.util.EJBSpringApplicationContextUtils;
-import br.com.afrcode.arquitetura.spring.config.security.AuthenticationProviderImpl;
 import br.com.afrcode.arquitetura.spring.config.security.UserDetailsServiceImpl;
 import br.com.afrcode.arquitetura.spring.config.util.Profiles;
 
@@ -34,17 +31,12 @@ public class AutenticadorInterceptor {
 	@Resource(name = "java:app/AppName")
 	private String appName;
 
-	private AuthenticationProviderImpl getAuthenticationProviderImpl() {
-		AuthenticationProviderImpl authenticationProviderImpl =
-				EJBSpringApplicationContextUtils
-						.getBean(AuthenticationProviderImpl.class);
-		return authenticationProviderImpl;
-	}
-
 	@AroundInvoke
 	public Object repassarCredenciais(InvocationContext ctx) throws Exception {
-		String username = EjbContextUtils.obterCredencial(ejbContext);
-		repassarCredenciais(username);
+		if (SecurityContextHolder.getContext().getAuthentication() == null) {
+			String username = EjbContextUtils.obterCredencial(ejbContext);
+			repassarCredenciais(username);
+		}
 		return ctx.proceed();
 	}
 
@@ -68,17 +60,10 @@ public class AutenticadorInterceptor {
 						user.getPassword(), user.getAuthorities());
 
 		// Repassando credenciais ao Spring Security.
-		AuthenticationProviderImpl authenticationProviderImpl =
-				getAuthenticationProviderImpl();
-		Authentication authentication =
-				authenticationProviderImpl.authenticate(usrPasswdToken);
-		Validate.notNull(
-				authentication,
-				"Houve falha no repasse via EjbContext de credenciais de usuário ao Spring Security!");
-		SecurityContextHolder.getContext().setAuthentication(authentication);
+		SecurityContextHolder.getContext().setAuthentication(usrPasswdToken);
 		if (LOG.isDebugEnabled()) {
 			LOG.debug("Usuário obtido via EjbContext repassado ao Spring Security: "
-					+ authentication.getName());
+					+ usrPasswdToken.getName());
 		}
 	}
 
