49f85af357cdd7807b0f2bfd4ced7bbd4a2c7e8e
alysson.rodrigues@gmail.com <alysson.rodrigues@gmail.com@4c46430c-79e8-fb40-bfba-8e12dd562e6e>
2014-08-11 14:40:57 +0000

    Compartilhando ApplicationContext de aplicação com EJBs providos.

--- fw_arquitetura/fw_arquitetura_sist_is/src/main/java/br/com/afrcode/arquitetura/is/spring/config/ejb/SpringEjbAnnotationConfigApplicationContext.java
+++ fw_arquitetura/fw_arquitetura_sist_is/src/main/java/br/com/afrcode/arquitetura/is/spring/config/ejb/SpringEjbAnnotationConfigApplicationContext.java
@@ -10,13 +10,19 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class SpringEjbAnnotationConfigApplicationContext extends
 		AnnotationConfigApplicationContext {
-	public static final String CTX_SUFFIX = "_ejbSpringApplicationContext";
+    private static final Logger LOG = Logger.getLogger(SpringEjbAnnotationConfigApplicationContext.class);
 
-	public SpringEjbAnnotationConfigApplicationContext(String... basePackages) {
-		super();
-		setId(getId().concat(CTX_SUFFIX));
-		scan(basePackages);
-		refresh();
-	}
+    public SpringEjbAnnotationConfigApplicationContext(String... basePackages) {
+        super();
+        ApplicationContext appCtx = ApplicationContextUtils.getApplicationContext();
+        if (appCtx == null) {
+            appCtx = new AnnotationConfigApplicationContext(basePackages);
+            LOG.warn("ATENÇÃO: Um ApplicationContext Spring será iniciado especificamente para EJBs da aplicação [" +
+                    ApplicationContextUtils.getNomeAplicacao() + "] " +
+                    "Não foi possível recuperar um ApplicationContext Spring já iniciado para compartilhar com EJBs!");
+        }
+        setParent(appCtx);
+        refresh();
+    }
 
 }
