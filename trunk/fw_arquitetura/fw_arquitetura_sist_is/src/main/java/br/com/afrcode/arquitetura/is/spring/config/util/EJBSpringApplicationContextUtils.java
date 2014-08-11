ba95559bc99850616673ee4be4d2969f0dab053f
alysson.rodrigues@gmail.com <alysson.rodrigues@gmail.com@4c46430c-79e8-fb40-bfba-8e12dd562e6e>
2014-08-11 14:42:35 +0000

    Ajustes.

--- trunk/fw_arquitetura/fw_arquitetura_sist_is/src/main/java/br/com/afrcode/arquitetura/is/spring/config/util/EJBSpringApplicationContextUtils.java
+++ trunk/fw_arquitetura/fw_arquitetura_sist_is/src/main/java/br/com/afrcode/arquitetura/is/spring/config/util/EJBSpringApplicationContextUtils.java
@@ -1,45 +1,30 @@
 package br.com.afrcode.arquitetura.is.spring.config.util;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.access.BeanFactoryLocator;
-import org.springframework.beans.factory.access.BeanFactoryReference;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
+
+import br.com.afrcode.arquitetura.util.contexto.ApplicationContextUtils;
 
 /**
- * Classe utilitária para obteção do BeanFactory Spring através do qual é
- * possível acessar o contexto Spring para obtenção de beans e resources. Em
- * geral esta classe será usada onde o contexto Spring não é injetável
- * declarativamente, exemplo: InterceptorS EJB.
+ * Classe utilitaria para obteÃ§Ã£o do BeanFactory Spring atraves do qual e possivel acessar o contexto Spring
+ * para obtencao de beans e resources. Em geral esta classe serÃ¡ usada onde o contexto Spring nao e injetÃ¡vel declarativamente,
+ * exemplo: InterceptorS EJB.
  * 
  * 
  */
 public class EJBSpringApplicationContextUtils {
 
-	public static <T> T getBean(Class<T> tipoBean) {
-		return getApplicationContext().getBean(tipoBean);
-	}
+    public static BeanFactory getBeanFactory() {
+        return getApplicationContext();
+    }
 
-	public static BeanFactory getBeanFactory() {
-		BeanFactoryLocator beanFactoryLocator = ContextSingletonBeanFactoryLocator
-				.getInstance();
-		BeanFactoryReference beanFactoryReference = beanFactoryLocator
-				.useBeanFactory(null);
-		return beanFactoryReference.getFactory();
-	}
+    public static ApplicationContext getApplicationContext() {
+        return ApplicationContextUtils.getApplicationContext();
+    }
 
-	public static ApplicationContext getApplicationContext() {
-		ApplicationContext applicationContext = null;
-		BeanFactory beanFactory = getBeanFactory();
-		if (beanFactory instanceof ApplicationContext) {
-			applicationContext = (ApplicationContext) beanFactory;
-		} else {
-			throw new RuntimeException(
-					"O contexto Spring não foi iniciado adequadamente. "
-							+ "Verique o tipo deste contexto em: "
-							+ "fw_arquitetura_sist_is/src/main/resources/beanRefContext.xml");
-		}
-		return applicationContext;
-	}
+    public static <T> T getBean(Class<T> tipoBean) {
+        return getApplicationContext().getBean(tipoBean);
+    }
 
 }
+
