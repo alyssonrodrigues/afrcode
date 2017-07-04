981fde6288fcee8a14c6317d0b1c5343abb41c65
alysson <alysson@venus>
2017-07-04 17:21:06 -0300

    Carga inicial.

---
+++ webtmp/src/main/java/com/afrcode/springboot/WebAppConfiguration.java
@@ -0,0 +1,13 @@
+package com.afrcode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppConfiguration {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppConfiguration.class, args);
+	}
+
+}
