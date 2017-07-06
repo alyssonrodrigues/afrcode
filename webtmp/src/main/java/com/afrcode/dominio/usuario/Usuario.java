f2b43483d4c310fd528979996bef422dac089afa
alysson <alysson@venus>
2017-07-06 11:25:13 -0300

    Inclusão Spring Boot Data JPA. Inclusão lombok.

---
+++ webtmp/src/main/java/com/afrcode/dominio/usuario/Usuario.java
@@ -0,0 +1,40 @@
+package com.afrcode.dominio.usuario;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "USUARIO")
+@SequenceGenerator(name = "ID_GENERATOR", sequenceName = "SEQ_USUARIO")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+	@Version
+	private Long versao;
+
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "ID_GENERATOR")
+	@Id
+	@Column(name = "ID")
+	private Long id;
+
+	@Column(name = "NOME")
+	private String nome;
+
+	@Column(name = "LOGIN")
+	private String login;
+
+}
