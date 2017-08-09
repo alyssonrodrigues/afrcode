8d742ce03fa4815046bd92a4050f0c05c2db9cfe
alysson <alysson@venus>
2017-08-09 14:46:35 -0300

    SSDD

--- cursos/src/main/resources/static/jsavancado/client/js/app/models/NegociacoesList.js
+++ cursos/src/main/resources/static/jsavancado/client/js/app/models/NegociacoesList.js
@@ -14,4 +14,10 @@ class NegociacoesList {
 	get negociacoes() {
 		return [].concat(this._negociacoes);
 	}
+	
+	get volumeTotal() {
+		return this._negociacoes.reduce(
+        		(total, negociacao) => total + negociacao.volume, 
+        		0.0);
+	}
 }
