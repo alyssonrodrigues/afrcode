d3d5e620e0ca4931e0d4f62a0d467ee4f265dbe4
alysson <alysson@venus>
2017-08-14 09:59:20 -0300

    Fetch API.

--- cursos/src/main/resources/static/jsavancado/client/js/app/services/HttpService.js
+++ cursos/src/main/resources/static/jsavancado/client/js/app/services/HttpService.js
@@ -1,36 +1,19 @@
 class HttpService {
+	_handleErrors(result) {
+		if (!result.ok) throw new Error(result.statusText);
+		return result;
+	}
 	get(url) {
-		return new Promise((resolve, reject) => {
-	    	let xhr = new XMLHttpRequest();
-	    	xhr.open("GET", url);
-	    	xhr.onreadystatechange = () => {
-		    	if(xhr.readyState == 4) {
-		    		if (xhr.status == 200) {
-		    			resolve(JSON.parse(xhr.responseText));
-		    		} else {
-		    			reject(xhr.responseText);
-		    		}
-		    	}
-	    	};
-	    	xhr.send();
-		});
+		return fetch(url)
+			.then(result => this._handleErrors(result))
+			.then(result => result.json());
 	}
 	
     post(url, dado) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado));
-        });
+    	return fetch(url, {
+    		headers : {"Content-type" : "application/json"},
+    		method: "post",
+    		body: JSON.stringify(dado)})
+    		.then(result => this._handleErrors(result));
     }
 }
