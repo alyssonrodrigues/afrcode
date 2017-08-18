d81fd45b3818eac590c7b3d6714f03d4594cef5e
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-08-18 13:43:26 -0300

    SSDD

--- cursos/src/main/resources/static/angularjs/public/js/directives/custom-directives.js
+++ cursos/src/main/resources/static/angularjs/public/js/directives/custom-directives.js
@@ -5,13 +5,6 @@ angular.module("customDirectives", []).directive("customPanel", function () {
     ddo.scope = {
         titulo: "@"
     };
-    ddo.template = 
-          '<div class="panel panel-default">'
-        + '    <div class="panel-heading">'
-        + '        <h3 class="panel-title">{{titulo}}</h3>'
-        + '    </div>'
-        + '    <div class="panel-body" ng-transclude>'
-        + '    </div>'
-        + '</div>';
+    ddo.templateUrl = "js/directives/custom-panel.html";
     return ddo;
 });
