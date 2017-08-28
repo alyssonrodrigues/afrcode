439d8fda100f999ec4a14f144b96a257af122223
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-08-28 15:08:14 -0300

    Body parser

--- cursos/src/main/resources/static/nodejs/app/routes/routes.js
+++ cursos/src/main/resources/static/nodejs/app/routes/routes.js
@@ -7,7 +7,26 @@ module.exports = (app) => {
                 console.log(error);
             }
             console.log(result);
-            response.render('index.ejs', {
+            response.render('produtos', {
+                produtos: result
+            });
+        });
+        connection.end();
+    });
+
+    app.get('/produtos/inserir', (request, response) => response.render('cadastrar-produto'));
+
+    app.post('/produtos/salvar', (request, response) => {
+        var connection = app.services.connectionFactory();
+        var produtosService = new app.services.ProdutosService(connection);
+        var produto = request.body;
+        console.log(produto);
+        produtosService.recuperarTodos((error, result) => {
+            if (error) {
+                console.log(error);
+            }
+            console.log(result);
+            response.render('produtos', {
                 produtos: result
             });
         });
