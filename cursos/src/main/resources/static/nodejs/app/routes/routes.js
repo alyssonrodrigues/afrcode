7c526d1d92dad2d515eb6e92e1f69db85ebba084
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-08-28 10:36:17 -0300

    MySql

--- cursos/src/main/resources/static/nodejs/app/routes/routes.js
+++ cursos/src/main/resources/static/nodejs/app/routes/routes.js
@@ -1,5 +1,18 @@
 module.exports = (app) => {
     app.get('/', (request, response) => {
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'M03y08s14',
+            database: 'nodejsapp'
+        });
+        connection.query('select * from livro', (error, result) => {
+            if (error) {
+                console.log(error);
+            }
+            console.log(result);
+        })
         response.render('index.ejs');
     });
 };
