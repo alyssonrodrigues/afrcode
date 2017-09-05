81b321f68a29bf9f219a06000b539f5594963c87
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-09-05 10:27:27 -0300

    SSDD

--- cursos/src/main/resources/static/nodejs/app/services/connection-factory.js
+++ cursos/src/main/resources/static/nodejs/app/services/connection-factory.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
 const connectionFactory = () => {
+    var env = process.env.NODE_ENV || '';
     var connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'M03y08s14',
-        database: 'nodejsapp'
+        database: `nodejsapp${env}`
     });
     return connection;
 };
--- cursos/src/main/resources/static/nodejs/test/ProdutosControllerTest.js
+++ cursos/src/main/resources/static/nodejs/test/ProdutosControllerTest.js
@@ -1,24 +1,52 @@
-var http = require('http');
-var assert = require('assert');
+var app = require('../config/express')();
+var supertest = require('supertest')(app);
 
 describe('#ProdutosController', () => {
-    it('#recuperarTodos', (done) => {
-        http.get(
-            {
-                hostname: 'localhost',
-                port: 3000,
-                path: '/',
-                headers: {
-                    'Accept': 'application/json'
-                }
-            },
-            (response) => {
-                assert.equal(response.statusCode, 200);
-                assert.equal(response.headers['content-type'], 
-                    'application/json; charset=utf-8');
-                response.on('data', (body) => console.log(`${body}`));
-                done();
+    const removerTodos = (done) => {
+        let connection = app.services.connectionFactory();
+        connection.query('delete from produtos', (error, result) => {
+            if (error) {
+                console.log(error);
+                throw new Error(JSON.stringify(error));
             }
-        );
+            done();
+        });
+    };
+
+    beforeEach(removerTodos);
+    afterEach(removerTodos);
+
+    it('#recuperarTodosJSON', (done) => {
+        supertest.get('/')
+            .set('Accept', 'application/json')
+            .expect('Content-type', /json/)
+            .expect(200, done);
+    });
+
+    it('#recuperarTodosHTML', (done) => {
+        supertest.get('/')
+            .set('Accept', 'text/html')
+            .expect('Content-type', /html/)
+            .expect(200, done);
+    });
+
+    it('#salvarJSONProdutoInvalido', (done) => {
+        supertest.post('/')
+            .send({
+                titulo: '',
+                descricao: '',
+                preco: ''
+            })
+            .expect(400, done);
+    });
+
+    it('#salvarJSON', (done) => {
+        supertest.post('/')
+            .send({
+                titulo: 'Abobrinhas',
+                descricao: 'Sobre Abobrinhas',
+                preco: '1.55'
+            })
+            .expect(302, done);
     });
 });
