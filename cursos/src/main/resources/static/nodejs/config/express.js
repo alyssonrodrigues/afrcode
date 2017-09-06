f0152cfc9aa9423a3157a91f4b0f3b6eae8aac51
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-09-06 10:49:13 -0300

    express.static

--- cursos/src/main/resources/static/nodejs/config/express.js
+++ cursos/src/main/resources/static/nodejs/config/express.js
@@ -6,6 +6,7 @@ var expressValidator = require('express-validator');
 module.exports = () => {
     var app = express();
 
+    app.use('/static', express.static('public'));
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
     app.use(bodyParser.urlencoded({
