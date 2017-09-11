0d84fdbadacd5f5b7824e904402cd8e444e3c3b8
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-09-11 14:20:12 -0300

    express-validator

--- cursos/src/main/resources/static/rest/config/express.js
+++ cursos/src/main/resources/static/rest/config/express.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var consign = require('consign');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = () => {
     var app = express();
+    
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(expressValidator());
+
     consign()
         .include('app/routes')
         .into(app);
