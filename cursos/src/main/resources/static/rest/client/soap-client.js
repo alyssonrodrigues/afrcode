53b3f85b4bd9c527dd11dc75ce316d739653936b
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-09-13 14:33:51 -0300

    SSDD

--- cursos/src/main/resources/static/rest/client/soap-client.js
+++ cursos/src/main/resources/static/rest/client/soap-client.js
@@ -1,16 +1,14 @@
 var soap = require('soap');
 var url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
 
-soap.createClient(url, 
-    (error, client) => {
-        client.CalcPrazo(
-            {
-                'nCdServico': '40010',
-                'sCepOrigem': '04101300',
-                'sCepDestino': '65000600'
-            }, 
-            (error, result) => {
-                console.log(JSON.stringify(result));
-        }
-    );
+var dados = {
+    'nCdServico': '40010',
+    'sCepOrigem': '04101300',
+    'sCepDestino': '65000600'
+};
+
+soap.createClient(url, (error, client) => {
+    client.CalcPrazo(dados, (error, result) => {
+        console.log(JSON.stringify(result));
+    });
 });
