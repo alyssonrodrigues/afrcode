b8c1f83a917a0f45ec93f06823a40676742f1db5
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-09-13 09:18:04 -0300

    SSDD

--- cursos/src/main/resources/static/rest/app/routes/routes.js
+++ cursos/src/main/resources/static/rest/app/routes/routes.js
@@ -1,7 +1,7 @@
 module.exports = (app) => {
     var pagamentosController = new app.controllers.PagamentosController(app);
-    app.get('/', pagamentosController.recuperarTodos.bind(pagamentosController));
-    app.post('/', pagamentosController.salvar.bind(pagamentosController));
-    app.get('/pagamento/:pagamentoId', pagamentosController.recuperarPorId.bind(pagamentosController));
-    app.get('/remover/:pagamentoId', pagamentosController.remover.bind(pagamentosController));
+    app.get('/api/pagamentos', pagamentosController.recuperarTodos.bind(pagamentosController));
+    app.post('/api/pagamentos', pagamentosController.salvar.bind(pagamentosController));
+    app.get('/api/pagamento/:pagamentoId', pagamentosController.recuperarPorId.bind(pagamentosController));
+    app.get('/api/pagamento/remover/:pagamentoId', pagamentosController.remover.bind(pagamentosController));
 };
