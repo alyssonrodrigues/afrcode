84e9a0417985e0ac9577ed44f14e9af2c821c280
Alysson Félix Rodrigues <alyssonfr@tcu.gov.br>
2017-11-09 15:43:26 -0200

    SSDD

--- cursos/spectacle/presentation/index.js
+++ cursos/spectacle/presentation/index.js
@@ -50,24 +50,25 @@ preloader(images);
 
 const theme = createTheme({
     primary: "white",
-    secondary: "#1F2022",
-    tertiary: "#03A9FC",
-    quartenary: "#CECECE"
+    black: "#1F2022",
+    lightBlue: "#03A9FC",
+    lightGray: "#CECECE",
+    red: "red"
 }, {
     primary: "Montserrat",
-    secondary: "Helvetica"
+    black: "Helvetica"
 });
 
 export default class Presentation extends React.Component {
     render() {
         return (
             <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
-                <Slide transition={["zoom"]} bgColor="secondary">
+                <Slide transition={["zoom"]} bgColor="black">
                     <Heading size={1} fit caps lineHeight={1} textColor="primary">React</Heading>
-                    <Text margin="10px 0 0" textColor="tertiary" fit bold>is it a worth?</Text>
+                    <Text margin="10px 0 0" textColor="lightBlue" fit bold>is it a worth?</Text>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">React usage statistics</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">React usage statistics</Heading>
                     <List>
                         <ListItem margin="20px 0 0" textSize="x-large">
                             <Link href="https://stackshare.io/react/in-stacks/" target="_blank" textColor="primary">
@@ -99,22 +100,22 @@ export default class Presentation extends React.Component {
                         </ListItem>
                     </List>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary">
+                <Slide transition={["fade"]} bgColor="black">
                     <Text textColor="primary" fit bold>
-                        This presentation is using <Link href="http://formidable.com/open-source/spectacle/" target="_blank" textColor="tertiary">Spectacle</Link>
+                        This presentation is using <Link href="http://formidable.com/open-source/spectacle/" target="_blank" textColor="lightBlue">Spectacle</Link>
                     </Text>
-                    <Text margin="30px 0 0" textColor="tertiary" textSize="x-large" bold>
+                    <Text margin="30px 0 0" textColor="lightBlue" textSize="x-large" bold>
                         A React based library for creating sleek presentations
                     </Text>
                     <Image src={images.nodeLogo} margin="30px 0 0" width="25%" height="25%"/>
-                    <Text margin="30px 0 0" textColor="tertiary" textSize="x-large" bold>
+                    <Text margin="30px 0 0" textColor="lightBlue" textSize="x-large" bold>
                         Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine
                     </Text>
                     <Text margin="10px 0 0" textColor="primary" textSize="large" bold>
                         Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world
                     </Text>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="tertiary">
+                <Slide transition={["fade"]} bgColor="lightBlue">
                     <Heading size={5} textColor="primary">What is React?</Heading>
                     <BlockQuote>
                         <Quote textColor="primary" textSize="xx-large">
@@ -123,15 +124,15 @@ export default class Presentation extends React.Component {
                         </Quote>
                     </BlockQuote>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={4} textColor="tertiary">Prior knowledge</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={4} textColor="lightBlue">Prior knowledge</Heading>
                     <Text margin="30px 0 0" textColor="primary" caps bold>JavaScript - ES6</Text>
                 </Slide>
-                <Slide transition={["zoom"]} bgColor="tertiary">
+                <Slide transition={["zoom"]} bgColor="lightBlue">
                     <Heading size={1} fit caps lineHeight={1} textColor="primary">Fundamentals</Heading>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={4} textColor="tertiary">Why React?</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={4} textColor="lightBlue">Why React?</Heading>
                     <List>
                         <ListItem margin="20px 0 0" textSize="xx-large">Complexity of two-way data binding</ListItem>
                         <ListItem margin="20px 0 0" textSize="xx-large">
@@ -145,7 +146,7 @@ export default class Presentation extends React.Component {
                         </ListItem>
                     </List>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <BlockQuote>
                         <Quote textSize="xx-large">
                             MV* did not scale well for Facebook’s huge codebase
@@ -157,10 +158,10 @@ export default class Presentation extends React.Component {
                         </Quote>
                     </BlockQuote>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <Image src={images.mvc}/>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <BlockQuote>
                         <Quote textSize="xx-large">
                             In general the flow inside the MV* pattern is not well defined. A lot of the bigger
@@ -168,28 +169,28 @@ export default class Presentation extends React.Component {
                         </Quote>
                     </BlockQuote>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="tertiary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="lightBlue" textColor="primary">
                     <BlockQuote>
                         <Quote textSize="xx-large">React challenges established best practices in tradicional MV* Frameworks</Quote>
                     </BlockQuote>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <BlockQuote>
                         <Quote textSize="xx-large">
                             Flux Pattern: unidirectional flow of data between a system’s components
                         </Quote>
-                        <Quote margin="20px 0 0" textSize="xx-large" textColor="tertiary">
+                        <Quote margin="20px 0 0" textSize="xx-large" textColor="lightBlue">
                             Flux keeps things more predictable than MV*
                         </Quote>
                     </BlockQuote>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <Image src={images.flux}/>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <Image src={images.fluxComplex}/>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <BlockQuote>
                         <Quote textSize="xx-large">
                             Controller, ModelView, and View are coupled: when you change one, you often have to change
@@ -197,8 +198,8 @@ export default class Presentation extends React.Component {
                         </Quote>
                     </BlockQuote>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">Build components, not templates</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">Build components, not templates</Heading>
                     <List>
                         <ListItem margin="20px 0 0" textSize="xx-large">UI Components are the cohesive units</ListItem>
                         <ListItem margin="20px 0 0" textSize="xx-large">
@@ -210,12 +211,12 @@ export default class Presentation extends React.Component {
                         </ListItem>
                     </List>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <Table>
                         <TableRow>
                             <TableHeaderItem padding="10px" textSize="xx-large"/>
                             <TableHeaderItem padding="10px" textSize="xx-large">Templates</TableHeaderItem>
-                            <TableHeaderItem padding="10px" textSize="xx-large" textColor="tertiary">
+                            <TableHeaderItem padding="10px" textSize="xx-large" textColor="lightBlue">
                                 React Components
                             </TableHeaderItem>
                         </TableRow>
@@ -238,18 +239,18 @@ export default class Presentation extends React.Component {
                         </TableRow>
                     </Table>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="tertiary" textColor="secondary">
+                <Slide transition={["fade"]} bgColor="lightBlue" textColor="black">
                     <CodePane lang="js" source={require("raw-loader!../assets/component.example")}/>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="tertiary" textColor="secondary">
+                <Slide transition={["fade"]} bgColor="lightBlue" textColor="black">
                     <CodePane lang="js" source={require("raw-loader!../assets/component-with-no-jsx.example")}/>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">Component Life Cycle</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">Component Life Cycle</Heading>
                     <Image src={images.reactLifeCycle}/>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">Props</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">Props</Heading>
                     <List>
                         <ListItem margin="20px 0 0" textSize="x-large">
                             Passed down from parent to child component
@@ -260,13 +261,13 @@ export default class Presentation extends React.Component {
                         <ListItem margin="20px 0 0" textSize="x-large">
                             Accessed via <S type="bold">this.props</S>
                         </ListItem>
-                        <ListItem margin="20px 0 0" textSize="x-large">
+                        <ListItem margin="20px 0 0" textSize="x-large" textColor="red">
                             Properties are immutable
                         </ListItem>
                     </List>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">State</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">State</Heading>
                     <List>
                         <ListItem margin="20px 0 0" textSize="x-large">
                             Represents internal state of the component
@@ -274,10 +275,10 @@ export default class Presentation extends React.Component {
                         <ListItem margin="20px 0 0" textSize="x-large">
                             Accessed via <S type="bold">this.state</S>
                         </ListItem>
-                        <ListItem margin="20px 0 0" textSize="x-large">
+                        <ListItem margin="20px 0 0" textSize="x-large" textColor="red">
                             Treat <S type="bold">this.state</S> as if it were immutable
                         </ListItem>
-                        <ListItem margin="20px 0 0" textSize="x-large">
+                        <ListItem margin="20px 0 0" textSize="x-large" textColor="red">
                             Modify <S type="bold">this.state</S> using <S type="bold">this.setState</S>
                         </ListItem>
                         <ListItem margin="20px 0 0" textSize="x-large">
@@ -286,38 +287,38 @@ export default class Presentation extends React.Component {
                         </ListItem>
                     </List>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">React</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">React</Heading>
                     <Text margin="30px 0 0" textColor="primary" fit bold>
                         Re-render everything on every update, sounds expensive?
                     </Text>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">Virtual DOM</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">Virtual DOM</Heading>
                     <Text margin="30px 0 0" textColor="primary" fit bold>
                         Whenever data changes, React builds a new virtual DOM subtree
                     </Text>
                     <List>
-                        <ListItem margin="20px 0 0" textSize="x-large">diffs it with the old one</ListItem>
+                        <ListItem margin="20px 0 0" textSize="x-large" textColor="red">diffs it with the old one</ListItem>
                         <ListItem margin="20px 0 0" textSize="x-large">
                             computes the minimal set of DOM mutations and puts them in a queue
                         </ListItem>
-                        <ListItem margin="20px 0 0" textSize="x-large">and batch executes all updates</ListItem>
+                        <ListItem margin="20px 0 0" textSize="x-large" textColor="red">and batch executes all updates</ListItem>
                         <ListItem margin="20px 0 0" textSize="x-large">and it is fast</ListItem>
                     </List>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
                     <Link href="https://github.com/facebook/react-devtools" target="_blank">
-                        <Heading size={5} textColor="tertiary">React Dev Tools</Heading>
+                        <Heading size={5} textColor="lightBlue">React Dev Tools</Heading>
                     </Link>
                     <Image src={images.reactDevTools}/>
                 </Slide>
-                <Slide transition={["zoom"]} bgColor="tertiary">
+                <Slide transition={["zoom"]} bgColor="lightBlue">
                     <Heading size={1} fit caps lineHeight={1} textColor="primary">Ready to React?</Heading>
                     <CodePane lang="bash" source={require("raw-loader!../assets/create-react-app.example")}/>
                 </Slide>
-                <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-                    <Heading size={5} textColor="tertiary">Live demos</Heading>
+                <Slide transition={["fade"]} bgColor="black" textColor="primary">
+                    <Heading size={5} textColor="lightBlue">Live demos</Heading>
                     <List>
                         <ListItem margin="20px 0 0" textSize="x-large">
                             <Link href="https://reactjs.org/" target="_blank" textColor="primary">
@@ -326,7 +327,7 @@ export default class Presentation extends React.Component {
                         </ListItem>
                     </List>
                 </Slide>
-                <Slide transition={["zoom"]} bgColor="tertiary">
+                <Slide transition={["zoom"]} bgColor="lightBlue">
                     <CodePane lang="js" source={require("raw-loader!../assets/mensagem-final.example")}/>
                 </Slide>
             </Deck>
