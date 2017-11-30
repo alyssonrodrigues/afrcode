0fc2e6182d79b3e57d86846555d5c81a9c0c1e41
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2017-11-30 14:32:54 -0200

    react-router

--- cursos/vue-react-angular-intro/react-fw-intro/src/App.js
+++ cursos/vue-react-angular-intro/react-fw-intro/src/App.js
@@ -1,97 +1,22 @@
 import React, { Component } from 'react';
-import AddedHobby from './components/AddedHobby';
-import DeletedHobby from './components/DeletedHobby';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Menu from './components/Menu';
+import Hobbies from './components/Hobbies';
+import HelloWorld from './components/HelloWorld';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      hobbies: ['Running', 'Dancing', 'Singing'],
-      deletedHobbies: [],
-      newHobby: ''
-    };
-  }
-
-  addNewHobby() {
-    this.setState(
-      prevState => {
-        return {
-          hobbies: prevState.hobbies.concat(prevState.newHobby),
-          newHobby: ''
-        }
-      }
-    );
-  }
-
-  removeHobby(hobby) {
-    this.setState(
-      prevState => {
-        return {
-          hobbies: prevState.hobbies.filter(h => h !== hobby),
-          deletedHobbies: prevState.deletedHobbies.concat(hobby)
-        }
-      }
-    );
-  }
-
-  readdHobby(deletedHobby) {
-    this.setState(
-      prevState => {
-        return {
-          hobbies: prevState.hobbies.concat(deletedHobby),
-          deletedHobbies: prevState.deletedHobbies.filter(h => h !== deletedHobby)
-        }
-      }
-    );
-  }
-
-  setNewHobby(event) {
-    this.setState({
-      newHobby: event.target.value
-    });
-  }
-
   render() {
     return (
-      <div id="app">
-        <input type="text" value={this.state.newHobby} 
-          onChange={this.setNewHobby.bind(this)}/>
-        <button onClick={this.addNewHobby.bind(this)}>Add new hobby</button>
-
-        {
-          this.state.hobbies.length ? 
-            <p>New hobbies: {this.state.hobbies.length}</p> : ''
-        }
-
-        <ul>
-          {
-            this.state.hobbies.map(
-              hobby => 
-                <AddedHobby 
-                  key={hobby}
-                  hobby={hobby} 
-                  removeHobby={this.removeHobby.bind(this)}/>
-            )
-          }
-        </ul>
-
-        {
-          this.state.deletedHobbies.length ? 
-            <p>Old hobbies: {this.state.deletedHobbies.length}</p> : ''
-        }
-
-        <ul>
-          {
-            this.state.deletedHobbies.map(
-              deletedHobby => 
-                <DeletedHobby 
-                  key={deletedHobby}
-                  hobby={deletedHobby} 
-                  readdHobby={this.readdHobby.bind(this)} />
-            )
-          }
-        </ul>
-      </div>
+      <BrowserRouter>
+        <div id="app">
+          <Switch>
+            <Route exact path="/" component={Hobbies} />
+            <Route path="/helloworld" component={HelloWorld} />
+          </Switch>
+          <Menu/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ cursos/vue-react-angular-intro/react-fw-intro/src/components/Hobbies.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+
+import AddedHobby from './AddedHobby';
+import DeletedHobby from './DeletedHobby';
+
+class Hobbies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hobbies: ['Running', 'Dancing', 'Singing'],
+      deletedHobbies: [],
+      newHobby: ''
+    };
+  }
+
+  addNewHobby() {
+    this.setState(
+      prevState => {
+        return {
+          hobbies: prevState.hobbies.concat(prevState.newHobby),
+          newHobby: ''
+        }
+      }
+    );
+  }
+
+  removeHobby(hobby) {
+    this.setState(
+      prevState => {
+        return {
+          hobbies: prevState.hobbies.filter(h => h !== hobby),
+          deletedHobbies: prevState.deletedHobbies.concat(hobby)
+        }
+      }
+    );
+  }
+
+  readdHobby(deletedHobby) {
+    this.setState(
+      prevState => {
+        return {
+          hobbies: prevState.hobbies.concat(deletedHobby),
+          deletedHobbies: prevState.deletedHobbies.filter(h => h !== deletedHobby)
+        }
+      }
+    );
+  }
+
+  setNewHobby(event) {
+    this.setState({
+      newHobby: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div id="hobbies">
+        <input type="text" value={this.state.newHobby} 
+          onChange={this.setNewHobby.bind(this)}/>
+        <button onClick={this.addNewHobby.bind(this)}>Add new hobby</button>
+
+        {
+          this.state.hobbies.length ? 
+            <p>New hobbies: {this.state.hobbies.length}</p> : ''
+        }
+
+        <ul>
+          {
+            this.state.hobbies.map(
+              hobby => 
+                <AddedHobby 
+                  key={hobby}
+                  hobby={hobby} 
+                  removeHobby={this.removeHobby.bind(this)}/>
+            )
+          }
+        </ul>
+
+        {
+          this.state.deletedHobbies.length ? 
+            <p>Old hobbies: {this.state.deletedHobbies.length}</p> : ''
+        }
+
+        <ul>
+          {
+            this.state.deletedHobbies.map(
+              deletedHobby => 
+                <DeletedHobby 
+                  key={deletedHobby}
+                  hobby={deletedHobby} 
+                  readdHobby={this.readdHobby.bind(this)} />
+            )
+          }
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Hobbies;
