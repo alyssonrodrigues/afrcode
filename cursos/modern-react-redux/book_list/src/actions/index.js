bd50f5966fad13749d6a00165df8f3b0c5b7c879
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-03-24 20:42:12 -0300

    SSDD

--- cursos/modern-react-redux/book_list/src/actions/index.js
+++ cursos/modern-react-redux/book_list/src/actions/index.js
@@ -1,3 +1,5 @@
+// Each action created by a ActionCreater is dispatched to all
+// reducers.
 export function selectBook(book) {
   // selectBook is an ActionCreator, it needs to return an action,
   // an object with a type property.
--- cursos/modern-react-redux/book_list/src/reducers/index.js
+++ cursos/modern-react-redux/book_list/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import BooksReducer from "./reducer_books";
 import ActiveBook from "./reducer_active_book";
 
+// Each reducer is reponsible for creating a single piece of
+// the application state.
 const rootReducer = combineReducers({
-  books: BooksReducer,
-  activeBook: ActiveBook
+  books: BooksReducer, // reduces aplication state.books
+  activeBook: ActiveBook // reduces application state.activeBook
 });
 
 export default rootReducer;
