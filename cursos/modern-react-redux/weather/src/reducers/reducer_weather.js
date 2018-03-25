3c57bc70cf8030ff49276c78058cd8ccb9613838
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-03-25 17:26:51 -0300

    SSDD

--- cursos/modern-react-redux/weather/src/reducers/reducer_weather.js
+++ cursos/modern-react-redux/weather/src/reducers/reducer_weather.js
@@ -3,6 +3,7 @@ import { FETCH_WEATHER } from "../actions/index";
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
+      // reducers must return a new fresh state object with new data added!
       return [action.payload.data, ...state];
   }
   return state;
