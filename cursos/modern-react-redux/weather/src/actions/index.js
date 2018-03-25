9a9c3d31751d04e2601160147c1f9b3f8823d55e
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-03-24 21:58:46 -0300

    SSDD

--- cursos/modern-react-redux/weather/src/actions/index.js
+++ cursos/modern-react-redux/weather/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "6a78596d062df78380eff5944c4e5567";
+const API_KEY = "00581aeb65e9b44282bf60da51dc590d";
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
