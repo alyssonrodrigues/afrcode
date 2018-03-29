87e116d9c9daedc1da2d6e5bd27f6582e187aaba
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-03-29 12:24:15 -0300

    ReduxDevTools

--- cursos/modern-react-redux/book_list/src/index.js
+++ cursos/modern-react-redux/book_list/src/index.js
@@ -6,8 +6,15 @@ import { createStore } from "redux";
 import App from "./components/app";
 import rootReducer from "./reducers/index";
 
+// Creates a Redux store that holds the complete state tree of your app.
+// There should only be a single store in your app.
+const store = createStore(
+  rootReducer,
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
 ReactDOM.render(
-  <Provider store={createStore(rootReducer)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector(".container")
