a38255026eb39c933d4c15dcea5976b98d95573e
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-04-05 10:01:39 -0300

    help

--- react-redux-starter-kit/src/components/AppIndex.js
+++ react-redux-starter-kit/src/components/AppIndex.js
@@ -9,8 +9,15 @@ const AppIndex = () => (
       <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Bem vindo ao React</h1>
     </header>
+    <br/>
     <p className="App-intro">
       Para iniciar, edite o <code>src/AppIndex.js</code> e salve para recarregar.
+    </p>
+    <p className="App-intro">
+      Veja também o <code>src/routes/AppRoutes.js</code> para definições de "routes".
+    </p>
+    <p className="App-intro">
+      Encontre em <code>src/util/applicationContext.js</code> configurações de contexto da aplicação.
     </p>
     <p>
       <Link className="btn btn-link" to='/logout'>Sair</Link>
