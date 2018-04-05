6041176baca22645f9a7246af3559f5d7e3a0c59
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-04-05 09:55:58 -0300

    misc...

--- react-redux-starter-kit/src/components/AppIndex.js
+++ react-redux-starter-kit/src/components/AppIndex.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import './AppIndex.css';
 
@@ -6,10 +7,13 @@ const AppIndex = () => (
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
+      <h1 className="App-title">Bem vindo ao React</h1>
     </header>
     <p className="App-intro">
-      To get started, edit <code>src/AppIndex.js</code> and save to reload.
+      Para iniciar, edite o <code>src/AppIndex.js</code> e salve para recarregar.
+    </p>
+    <p>
+      <Link className="btn btn-link" to='/logout'>Sair</Link>
     </p>
   </div>
 );
--- react-redux-starter-kit/src/components/Logout.js
+++ react-redux-starter-kit/src/components/Logout.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import { logoutUser } from '../actions/authenticationJwtActions';
 
-class Logout extends Component {
-  render() {
-    console.log(this.props);
-    this.props.logoutUser();
-    return (<Redirect to='/' />);
-  }
-
+const Logout = (props) => {
+  props.logoutUser();
+  return (<Redirect to='/' />);
 };
 
 export default connect(null, { logoutUser })(Logout);
