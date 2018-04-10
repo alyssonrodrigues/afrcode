c6352380fd25ddbe55573294b38bcdbbfae27895
Alysson Felix Rodrigues (ALYSSONFR) <alyssonfr@tcu.gov.br>
2018-04-10 09:38:49 -0300

    AppIndex devTips

--- react-redux-starter-kit/src/components/AppIndex.js
+++ react-redux-starter-kit/src/components/AppIndex.js
@@ -3,13 +3,8 @@ import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import './AppIndex.css';
 
-const AppIndex = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Bem vindo ao React</h1>
-    </header>
-    <br/>
+const devTips = process.env.NODE_ENV === 'production' ? '' : (
+  <div>
     <p className="App-intro">
       Para iniciar, edite o <code>src/AppIndex.js</code> e salve para recarregar.
     </p>
@@ -19,6 +14,19 @@ const AppIndex = () => (
     <p className="App-intro">
       Encontre em <code>src/util/applicationContext.js</code> configurações de contexto da aplicação.
     </p>
+    <p className="App-intro">
+      Encontre em <code>src/interceptors</code> "interceptors" para aspectos comuns da aplicação.
+    </p>
+  </div>);
+
+const AppIndex = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Bem vindo ao React</h1>
+    </header>
+    <br/>
+    {devTips}
     <p>
       <Link className="btn btn-link" to='/logout'>Sair</Link>
     </p>
