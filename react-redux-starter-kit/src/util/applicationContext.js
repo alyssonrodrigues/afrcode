a79f780dc6831ed85a034797af38f20933527379
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-04-10 19:09:46 -0300

    jwt-authentication

--- react-redux-starter-kit/src/util/applicationContext.js
+++ react-redux-starter-kit/src/util/applicationContext.js
@@ -1,11 +1,6 @@
-export const codRecursoComputacional = 201
-
-export const authenticationUrl = process.env.NODE_ENV === 'production'
-  ? 'http://autenticacao-rest-prod.cloudapps.tcu.gov.br/auth/token-jwt'
-  : 'http://autenticacao-rest.desenv.rancher.tcu.gov.br/auth/token-jwt'
+export const authenticationUrl = 'http://localhost:8080/auth/token-jwt'
 
 export const createDataToAuthentication = (username, password) => ({
-  codRecursoComputacional,
   username,
   password
 })
