33996f3e3d09caa4cf6b1281debae60963bf7d33
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-04-10 19:17:04 -0300

    SSDD

--- jwt-authentication/server.js
+++ jwt-authentication/server.js
@@ -1,9 +1,6 @@
 var app = require('./config/express')();
 var http = require('http').Server(app);
 
-var io = require('socket.io')(http);
-app.set('io', io);
-
 var serverPort = 8080;
 var serverHostname = 'localhost';
 http.listen(serverPort, serverHostname, () => {
