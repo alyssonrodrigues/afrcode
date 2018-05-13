72cb403077178fde2aa9bcc2ede7c3f546ebb2fd
Alysson Rodrigues <alysson.rodrigues@gmail.com>
2018-05-12 21:00:53 -0300

    SSDD

--- react-redux-starter-kit/src/components/Logout.js
+++ react-redux-starter-kit/src/components/Logout.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
 import { logoutUser } from '../actions/authenticationJwtActions'
+import { getAuthentication } from '../security/securityContext'
+import { showSuccessMsg } from '../util/messagesUtil'
 
-export const Logout = props => {
-  props.logoutUser()
-  return (<Redirect to='/' />)
+export class Logout extends Component {
+  componentWillMount () {
+    const authentication = getAuthentication()
+    if (authentication && authentication.isUserAuthenticated) {
+      showSuccessMsg(`Até breve ${authentication.username}!`)
+    }
+  }
+
+  render () {
+    this.props.logoutUser()
+    return (<Redirect to='/' />)
+  }
 }
 
 export default connect(null, { logoutUser })(Logout)
