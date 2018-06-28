1f68e5beb02a46136527aa46f163eb6a7eaba1b2
Alysson Felix Rodrigues (ALYSSONFR) <alyssonfr@tcu.gov.br>
2018-06-28 10:52:40 -0300

    Impl. tratamento de erros.

--- react-redux-starter-kit/src/components/ForbiddenAccess.js
+++ react-redux-starter-kit/src/components/ForbiddenAccess.js
@@ -1,16 +1,77 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContenText from '@material-ui/core/DialogContentText'
+import DialogActions from '@material-ui/core/DialogActions'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
-import App from './App'
+import { dismissMessage } from '../actions/messagesActions'
 
-// TODO refinar página de acesso negado
-const ForbiddenAccess = () => (
-  <App noAppToolbar>
-    <h4>
-      403 Acesso negado
-    </h4>
-    <Link to='/'>Retornar</Link>
-  </App>
-)
+const styles = {
+  paperClassName: {
+    borderRadius: '2%',
+    outline: 'none'
+  },
+  backdropClassName: {
+    backgroundColor: 'rgba(255, 255, 255, 0.65)'
+  },
+  item: {
+    paddingTop: 12
+  },
+  link: {
+    padding: 12
+  }
+}
 
-export default ForbiddenAccess
+class ForbiddenAccess extends Component {
+  componentWillUnmount () {
+    this.props.dismissMessage()
+  }
+
+  render () {
+    const { classes, history } = this.props
+    return (
+      <main>
+        <Dialog open
+          BackdropProps={{className: classes.backdropClassName}}
+          PaperProps={{className: classes.paperClassName, elevation: 2}}>
+          <DialogTitle disableTypography>
+            <Typography variant='display1' color='error'>
+              Acesso negado
+            </Typography>
+          </DialogTitle>
+          <DialogContent>
+            <DialogContenText>
+              <Typography variant='title' style={{ maxWidth: 500 }}>
+                Você não tem permissão para acessar este recurso, solicite ao responsável, por favor.
+              </Typography>
+            </DialogContenText>
+            <DialogContenText>
+              <Typography variant='subheading' className={classes.item}>
+                Entre em contato através do 0800 644 1500 se o problema persistir.
+              </Typography>
+            </DialogContenText>
+          </DialogContent>
+          <DialogActions>
+            <Button className={classes.link} onClick={() => history.push('/')} color='primary'>
+              Voltar
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </main>
+    )
+  }
+}
+
+const materialUIEnhanced = withStyles(styles)(ForbiddenAccess)
+
+const mapStateToProps = ({ messages }) => ({ messages })
+
+const reduxEnhanced = connect(mapStateToProps, { dismissMessage })(materialUIEnhanced)
+
+export default withRouter(reduxEnhanced)
--- react-redux-starter-kit/src/components/HttpErrorView.js
+++ react-redux-starter-kit/src/components/HttpErrorView.js
@@ -1,16 +1,84 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles'
+import Dialog from '@material-ui/core/Dialog'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContenText from '@material-ui/core/DialogContentText'
+import DialogActions from '@material-ui/core/DialogActions'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 
-import App from './App'
+import { dismissMessage } from '../actions/messagesActions'
 
-// TODO refinar página de erro http genérico
-const HttpErrorView = () => (
-  <App noAppToolbar>
-    <h4>
-      Erro http genérico
-    </h4>
-    <Link to='/'>Retornar</Link>
-  </App>
-)
+const styles = {
+  paperClassName: {
+    borderRadius: '2%',
+    outline: 'none'
+  },
+  backdropClassName: {
+    backgroundColor: 'rgba(255, 255, 255, 0.65)'
+  },
+  item: {
+    paddingTop: 12
+  },
+  link: {
+    padding: 12
+  }
+}
 
-export default HttpErrorView
+class HttpErrorView extends Component {
+  componentWillUnmount () {
+    this.props.dismissMessage()
+  }
+
+  render () {
+    const { classes, messages: { message }, history } = this.props
+    return (
+      <main>
+        <Dialog open
+          BackdropProps={{className: classes.backdropClassName}}
+          PaperProps={{className: classes.paperClassName, elevation: 2}}>
+          <DialogTitle disableTypography>
+            <Typography variant='display1' color='error'>
+              Ocorreu um erro inesperado
+            </Typography>
+          </DialogTitle>
+          <DialogContent>
+            <DialogContenText>
+              <Typography variant='title'>
+                Aguarde alguns instantes e tente novamente.
+              </Typography>
+            </DialogContenText>
+            <DialogContenText>
+              <Typography variant='subheading' className={classes.item}>
+                Entre em contato através do 0800 644 1500 se o problema persistir.
+              </Typography>
+            </DialogContenText>
+            {message ? (
+              <DialogContenText>
+                <Typography variant='body1' className={classes.item} style={{ maxWidth: 800 }}>
+                  Mensagem: {`"${message.msgText}"`}
+                </Typography>
+              </DialogContenText>
+            ) : null}
+          </DialogContent>
+          <DialogActions>
+            <Button className={classes.link} onClick={() => history.push('/')} color='primary'>
+              Voltar
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </main>
+    )
+  }
+}
+
+const materialUIEnhanced = withStyles(styles)(HttpErrorView)
+
+const mapStateToProps = ({ messages }) => ({ messages })
+
+const reduxEnhanced = connect(mapStateToProps, { dismissMessage })(materialUIEnhanced)
+
+export default withRouter(reduxEnhanced)
