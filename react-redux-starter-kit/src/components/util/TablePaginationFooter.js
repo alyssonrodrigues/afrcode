e156c8067abd0a33a220b2cc5da909730a9a0ef0
Alysson Felix Rodrigues (ALYSSONFR) <alyssonfr@tcu.gov.br>
2018-10-01 16:25:11 -0300

    SSDD

--- react-redux-starter-kit/src/components/util/TablePaginationFooter.js
+++ react-redux-starter-kit/src/components/util/TablePaginationFooter.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import TablePagination from '@material-ui/core/TablePagination'
 import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
 import TableFooter from '@material-ui/core/TableFooter'
 
-const TablePaginationFooter = ({ count, page, initialRowsPerPage, rowsPerPage, handleChangePage, handleChangeRowsPerPage }) => (
+const TablePaginationFooter = ({
+  count,
+  page,
+  initialRowsPerPage,
+  rowsPerPage,
+  handleChangePage,
+  handleChangeRowsPerPage,
+  component: Component,
+  ...rest
+}) => (
   <TableFooter>
     <TableRow>
-      <TablePagination count={count} page={page} rowsPerPage={rowsPerPage}
+      <TablePagination
+        component={Component || TableCell}
+        count={count} page={page} rowsPerPage={rowsPerPage}
         labelRowsPerPage='Itens por página'
         labelDisplayedRows={({from, to, count}) => `${from}-${to} de ${count}`}
         rowsPerPageOptions={[initialRowsPerPage, initialRowsPerPage * 2, initialRowsPerPage * 3]}
         onChangePage={handleChangePage}
-        onChangeRowsPerPage={handleChangeRowsPerPage} />
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+        {...rest} />
     </TableRow>
   </TableFooter>
 )
