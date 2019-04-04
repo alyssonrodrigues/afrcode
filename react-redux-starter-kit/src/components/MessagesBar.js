143147545e1470b74311712cc120cd36141249f5
Alysson Felix Rodrigues (ALYSSONFR) <alyssonfr@tcu.gov.br>
2019-04-04 12:28:08 -0300

    SSDD

--- react-redux-starter-kit/src/components/MessagesBar.js
+++ react-redux-starter-kit/src/components/MessagesBar.js
@@ -4,15 +4,28 @@ import Snackbar from '@material-ui/core/Snackbar'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import { connect } from 'react-redux'
+import red from '@material-ui/core/colors/red'
+import blue from '@material-ui/core/colors/blue'
+import orange from '@material-ui/core/colors/orange'
+import green from '@material-ui/core/colors/green'
 
 import styles from './MessagesBarStyles'
 import { dismissMessage } from '../actions/messagesActions'
+
+const backgroundColors = {
+  error: red['A700'],
+  info: blue[500],
+  alert: orange[900],
+  success: green[500],
+  'default': blue[400],
+  'none': 'rgba(0, 0, 0, 0)'
+}
 
 export const MessagesBar = props => {
   const { classes } = props
   const { message } = props.messages
   const open = Boolean(message)
-  const className = open ? classes[message.type] : null
+  const backgroundColor = open ? backgroundColors[message.type] || backgroundColors['default'] : backgroundColors['none']
   const msgText = open ? message.msgText : null
   return (
     <Snackbar
@@ -20,10 +33,11 @@ export const MessagesBar = props => {
         vertical: 'bottom',
         horizontal: 'center'
       }}
+      ContentProps={{style: { backgroundColor }, elevation: open ? 6 : 0}}
       open={open}
       autoHideDuration={6000}
       onClose={props.dismissMessage}
-      message={<span id='message-id' className={className}>{msgText}</span>}
+      message={<span id='message-id'>{msgText}</span>}
       action={[
         <IconButton
           key='close'
--- react-redux-starter-kit/src/components/MessagesBarStyles.js
+++ react-redux-starter-kit/src/components/MessagesBarStyles.js
@@ -1,26 +1,6 @@
-import red from '@material-ui/core/colors/red'
-import yellow from '@material-ui/core/colors/yellow'
-import blue from '@material-ui/core/colors/blue'
-import lightGreen from '@material-ui/core/colors/lightGreen'
-
 export default theme => ({
   close: {
     width: theme.spacing.unit * 2.5,
     height: theme.spacing.unit * 2.5
-  },
-  error: {
-    color: red[500]
-  },
-  info: {
-    color: blue[400]
-  },
-  alert: {
-    color: yellow[500]
-  },
-  success: {
-    color: lightGreen['A400']
-  },
-  'default': {
-    color: blue[400]
   }
 })
