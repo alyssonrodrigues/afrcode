bd3019611aa0766403d8ce835024f20902bb3d20
Alysson Felix Rodrigues (ALYSSONFR) <alyssonfr@tcu.gov.br>
2019-05-14 11:27:11 -0300

    Correção de serviceWorker...

--- react-redux-starter-kit/src/index.js
+++ react-redux-starter-kit/src/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Root from './Root'
-import registerServiceWorker from './registerServiceWorker'
+import { unregister } from './registerServiceWorker'
 
 import './interceptors'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
-registerServiceWorker()
+unregister()
