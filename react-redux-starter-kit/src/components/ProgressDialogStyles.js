8a923a875647f6c4ebbe713f02ff30ecbdc27ad5
Alysson Felix Rodrigues (ALYSSONFR) <alyssonfr@tcu.gov.br>
2019-06-28 10:11:48 -0300

    SSDD

--- react-redux-starter-kit/src/components/ProgressDialogStyles.js
+++ react-redux-starter-kit/src/components/ProgressDialogStyles.js
@@ -1,6 +1,8 @@
 export default {
   paperClassName: {
-    outline: 'none'
+    outline: 'none',
+    borderRadius: 60,
+    padding: '18px 4px 16px 4px'
   },
   backdropClassName: {
     backgroundColor: 'rgba(255, 255, 255, 0.65)'
